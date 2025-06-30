1889c6b25c179fe94cba4e1b88bf905237cf18f2
Guney <guneysuleymanofa@gmail.com>
2025-06-30 10:45:49 +0400

    added some changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,14 @@ const About = () => {
       {/* bio section */}
       <section className="mb-16">
         <p className="text-lg text-secondary max-w-3xl mx-auto text-center">
-          I&apos;m bla bla
+          I&apos;m a front-end developer with a passion for building clean,
+          responsive, and user-friendly web interfaces. I have practical
+          experience working on real-world projects using HTML, CSS, JavaScript,
+          TypeScript, and modern frameworks like React and Next.js. I'm also
+          proficient in Tailwind CSS, which allows me to create consistent and
+          efficient UI designs. My goal is always to deliver intuitive,
+          accessible, and high-performance web applications that provide a
+          seamless user experience.
         </p>
       </section>
 
@@ -16,72 +23,79 @@ const About = () => {
       <section className="mb-16">
         <h2 className="section-title">Skills</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
-                <FaCode className="h-8 w-8 text-primary mb-4" />
-                <h3 className="text-xl font-semibold mb-2">Frontend</h3>
-                <ul className="text-secondary space-y-2">
-                  <li>React / Next.js</li>
-                  <li>Typescript</li>
-                  <li>Tailwind</li>
-                  <li>HTML5 / CSS3</li>
-                </ul>
-              </div>
+          <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
+            <FaCode className="h-8 w-8 text-primary mb-4" />
+            <h3 className="text-xl font-semibold mb-2">Frontend</h3>
+            <ul className="text-secondary space-y-2">
+              <li>React / Next.js</li>
+              <li>Typescript</li>
+              <li>Tailwind, Bootstrap</li>
+              <li>HTML5 / CSS3</li>
+              <li>Redux, jQuery</li>
+            </ul>
+          </div>
 
-               <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
-                <FaCode className="h-8 w-8 text-primary mb-4" />
-                <h3 className="text-xl font-semibold mb-2">Tools & Others</h3>
-                <ul className="text-secondary space-y-2">
-                  <li>Git / GitHub</li>
-                  <li>Docker</li>
-                  <li>AWS</li>
-                  <li>CI/CD</li>
-                </ul>
-              </div>
+          <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
+            <FaCode className="h-8 w-8 text-primary mb-4" />
+            <h3 className="text-xl font-semibold mb-2">Tools & Others</h3>
+            <ul className="text-secondary space-y-2">
+              <li>Git / GitHub</li>
+              <li>Docker</li>
+              <li>Figma</li>
+              <li>AWS</li>
+              <li>CI/CD</li>
+            </ul>
+          </div>
         </div>
       </section>
 
       {/* experience section */}
 
       <section className="mb-16">
-          <h2 className="section-title">Experiences</h2>
-          <div className="max-w-3xl mx-auto space-y-8">
-            <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-semibold mb-2">Frontend Developer</h3>
-              <p className="text-primary mb-2">Company Naame | Start Date - End Date</p>
-              <ul className="text-secondary list-disc list-inside space-y-2">
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, aspernatur?</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, aspernatur?</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, aspernatur?</li>
-              </ul>
-            </div>
-
-
-             <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-semibold mb-2">Frontend Developer</h3>
-              <p className="text-primary mb-2">Company Naame | Start Date - End Date</p>
-              <ul className="text-secondary list-disc list-inside space-y-2">
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, aspernatur?</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, aspernatur?</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, aspernatur?</li>
-              </ul>
-            </div>
-
+        <h2 className="section-title">Experiences</h2>
+        <div className="max-w-3xl mx-auto space-y-8">
+          <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
+            <h3 className="text-xl font-semibold mb-2">Frontend Developer</h3>
+            <p className="text-primary mb-2">
+              AFEA Group | 01/2025 - now
+            </p>
+            <ul className="text-secondary list-disc list-inside space-y-2">
+              <li>
+                Building responsive and user-friendly web interfaces.
+              </li>
+            </ul>
           </div>
+
+          <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
+            <h3 className="text-xl font-semibold mb-2">Frontend Developer</h3>
+            <p className="text-primary mb-2">HPC | 02/2024 - 06/2024</p>
+            <ul className="text-secondary list-disc list-inside space-y-2">
+              <li>
+                High-Performance Computing(HPC) at Azerbaijan State Oil and
+                Industry University
+                https://research.asoiu.edu.az/en/interdisciplinary-research/high-performance-computing-hpc/
+              </li>
+            </ul>
+          </div>
+        </div>
       </section>
 
       {/* Education section */}
 
       <section className="mb-16">
-          <h2 className="section-title">Education</h2>
-          <div className="max-w-3xl mx-auto space-y-8">
-            <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-semibold mb-2">Bachelor of Computer Science</h3>
-              <p className="text-primary mb-2">ASOIU University | 2020 - 2024</p>
-              <p className="text-secondary">Graduated with honors. Focused on software development. </p>
-            </div>
+        <h2 className="section-title">Education</h2>
+        <div className="max-w-3xl mx-auto space-y-8">
+          <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
+            <h3 className="text-xl font-semibold mb-2">
+              Bachelor of Computer Science
+            </h3>
+            <p className="text-primary mb-2">ASOIU University | 2020 - 2024</p>
+            <p className="text-secondary">
+              Graduated with honors. Focused on software development.{" "}
+            </p>
           </div>
+        </div>
       </section>
-
     </div>
   );
 };
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,18 +1,19 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 import { fadeInUp, scaleIn } from "@/utils/animation";
 const Hero = () => {
   return (
     <section className="py-28 container max-w-7xl mx-auto px-4">
       <div className="max-w-3xl mx-auto text-center">
         <motion.div
-        {...scaleIn}
-        transition={{delay : 0.2}}
-        className="flex flex-col items-center justify-center mb-4">
+          {...scaleIn}
+          transition={{ delay: 0.2 }}
+          className="flex flex-col items-center justify-center mb-4"
+        >
           <Image
             src="/guney.jpg"
             alt="profile image"
@@ -23,16 +24,18 @@ const Hero = () => {
           />
         </motion.div>
         <motion.h1
-        {...fadeInUp}
-        transition={{delay : 0.3}}
-        className="text-3xl md:text-5xl font-black mb-6">
-        Hi, I&apos;m <span className="text-primary">Guney Suleymanova</span>
+          {...fadeInUp}
+          transition={{ delay: 0.3 }}
+          className="text-3xl md:text-5xl font-black mb-6"
+        >
+          Hi, I&apos;m <span className="text-primary">Guney Suleymanova</span>
         </motion.h1>
 
         <motion.p
-        {...fadeInUp}
-        transition={{delay : 0.5}}
-        className="text-xl md:text-2xl text-gray-600 dark:text-gray-300 mb-8">
+          {...fadeInUp}
+          transition={{ delay: 0.5 }}
+          className="text-xl md:text-2xl text-gray-600 dark:text-gray-300 mb-8"
+        >
           Front-end Developer
         </motion.p>
 
@@ -45,18 +48,11 @@ const Hero = () => {
             <FaGithub />
           </Link>
           <Link
-            href="www.linkedin.com/in/guney-suleymanova-31265222b"
+            href="https://www.linkedin.com/in/guney-suleymanova-31265222b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
             target="_blank"
             className="text-2xl text-gray-600 dark:text-gray-300 hover:text-primary transition-colors duration-300"
           >
             <FaLinkedin />
-          </Link>
-          <Link
-            href="https://github.com/gguneii"
-            target="_blank"
-            className="text-2xl text-gray-600 dark:text-gray-300 hover:text-primary transition-colors duration-300"
-          >
-            <FaTwitter />
           </Link>
         </div>
 
